b094f9ef37651a81092da871f3cef2d144154753
Ashleigh Covello-Phillips <42661016+ashleigh835@users.noreply.github.com>
2022-09-14 10:30:32 -0500

    Dash! Ahhahh

--- dev.py
+++ dev.py
@@ -1,8 +1,12 @@
-from app.gui import GUI
+from app.cliGui import GUI
 
 gui = GUI()
 
+print('Thanks for using BudgetTool.py')
+# plotly==5.10.0
+# matplotlib==3.5.3
 # import pandas as pd
-# ax = df.plot(x = 'date',y = 'balance')
+# df = gui._Account_Manager._accounts[0]._transaction_df
+# ax = df.groupby('date', as_index=False).agg({'balance':'last'}).plot(x = 'date',y = 'balance')
 # fig = ax.get_figure()
 # fig.savefig('test.pdf')
